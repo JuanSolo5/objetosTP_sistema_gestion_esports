3f9bea09d2f6b4e2e8ae2f72b825db334d897256
JuanSolo5 <juanscazon@gmail.com>
2024-09-06 16:58:17 -0300

    Update sistema_gestion_esports.rb
    Creada clase Persona

--- sistema_gestion_esports.rb
+++ sistema_gestion_esports.rb
@@ -1,3 +1,23 @@
-#hola mundo
-#hola nico
-#No entiendo nada
+class Persona
+  def initialize(nombre, edad, correo_electronico, rol)
+    @nombre = nombre
+    @edad = edad
+    @correo_electronico = correo_electronico
+    @rol = rol
+  end
+  
+  def to_s
+    "Nombre: #{@nombre}, Edad: #{@edad}, Correo: #{@correo_electronico}, Rol: #{@rol} "
+  end
+
+  def mostrar_rol
+    puts rol
+  end
+
+end
+
+pepe = Persona.new("pepe", 2, "holacomoandas","bombero")
+
+puts(pepe)
+
+
