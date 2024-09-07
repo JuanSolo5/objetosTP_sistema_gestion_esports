4ee417c9ceb026fce77e853a2a84d6d773680946
nicohaluska <105892208+nicohaluska@users.noreply.github.com>
2024-09-07 00:23:25 -0300

    Update sistema_gestion_esports.rb

--- sistema_gestion_esports.rb
+++ sistema_gestion_esports.rb
@@ -1,9 +1,9 @@
 class Persona
-  def initialize(nombre, edad, correo_electronico, rol)
+  def initialize(nombre, edad, correo_electronico)
     @nombre = nombre
     @edad = edad
     @correo_electronico = correo_electronico
-    @rol = rol
+    @rol = ""
   end
   
   def to_s
@@ -14,13 +14,65 @@ class Persona
     puts rol
   end
 
+  def mostrar_nombre()
+    puts nombre
+  end
+
+  def agregarEquipo(equipo)
+    equipo.agregarEquipo(self)
+  end
 end
+
+require 'set'
+
+class Jugador < Persona
+  def initialize(nombre, edad, email, rango, jugadores_fav)
+    super(nombre, edad, email)
+    @rango = rango
+    @jugadores_fav = Set.new(jugadores_fav)  # Usamos un Set para asegurar jugadores únicos
+    @rol = "Jugador"
+  end
+
+  def to_s
+    "Nombre: #{@nombre}, Edad: #{@edad}, Email: #{@email}, Rol: #{@rol}, Rango: #{@rango}"
+  end
+
+  def get_rango
+    @rango
+  end
+
+  def set_rango(rango)
+    @rango = rango
+  end
+
+  def get_nombre
+    @nombre
+  end
+
+  def get_edad
+    @edad
+  end
+
+  def get_email
+    @email
+  end
+end
+
+
+
+
+
+
+
+
 
 pepe = Persona.new("pepe", 2, "holacomoandas","bombero")
 
 puts(pepe)
 puts "estoy escribiendo"
 
+
+=begin
 def generar_partidos(equipos) #ESTO VA DESPUES PARA GENERAR LOS PARTIDOS
   partidos = []
   n = equipos.length
@@ -31,3 +83,6 @@ def generar_partidos(equipos) #ESTO VA DESPUES PARA GENERAR LOS PARTIDOS
   end
   partidos
 end
+=end
+
+
